e803516ec10004cfa60813cded53ca2dc1d67ec3
Angel Arávalo <angelgabrielarevalo@gmail.com>
2022-08-18 22:08:07 -0300

    sdasd

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
 import { Server } from "./models/server.js";
 
+const a =10;
 dotenv.config();
 
 const server = new Server();
 
-server.listen();
+server.listen();    
